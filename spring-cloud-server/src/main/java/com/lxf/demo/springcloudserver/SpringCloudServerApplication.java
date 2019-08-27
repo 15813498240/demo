add4cbeb3984096435f001829784544cad3d2a54
15813498240 <15813498240@163.com>
2019-08-27 17:00:16 +0800

    ...

--- spring-cloud-server/src/main/java/com/lxf/demo/springcloudserver/SpringCloudServerApplication.java
+++ spring-cloud-server/src/main/java/com/lxf/demo/springcloudserver/SpringCloudServerApplication.java
@@ -1,15 +1,21 @@
 package com.lxf.demo.springcloudserver;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
 public class SpringCloudServerApplication {
+
+    @Autowired
+    RestTemplate restTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudServerApplication.class, args);
@@ -17,6 +23,12 @@ public class SpringCloudServerApplication {
 
     @RequestMapping("/hello")
     public String hello(){
-        return "hello world";
+        String forObject = restTemplate.getForObject("https://www.baidu.com/", String.class);
+        return "hello world ------------------------"+forObject;
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
     }
 }
