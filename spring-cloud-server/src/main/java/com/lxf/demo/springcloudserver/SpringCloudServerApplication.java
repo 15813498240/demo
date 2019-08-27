48ae11bb5c28dd16b97cd2886705cba67595d3c5
15813498240 <15813498240@163.com>
2019-08-27 11:10:27 +0800

    test项目启动

--- spring-cloud-server/src/main/java/com/lxf/demo/springcloudserver/SpringCloudServerApplication.java
+++ spring-cloud-server/src/main/java/com/lxf/demo/springcloudserver/SpringCloudServerApplication.java
@@ -2,12 +2,21 @@ package com.lxf.demo.springcloudserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
+@RestController
 public class SpringCloudServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudServerApplication.class, args);
     }
 
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
 }
